705070987b42347ca6d3aae40b608105bf3394d8
Umair Malik <80236241+Malikumair786@users.noreply.github.com>
2022-12-02 00:05:02 +0500

    Add files via upload

---
+++ lab mid Web/script.js
@@ -0,0 +1,14 @@
+
+// function increment(value){
+//     value++;
+// }
+// function decrement(value){
+//     // value--;
+//     if(value !== 0){
+//         value--;
+//     }
+// }
+
+function sweets{
+    document.getElementById(sweet).innerHTML = 
+}
